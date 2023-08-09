4f35e61de764ef5b70f669b82f45a1379b54faab
Beloyara <beloyara@inbox.ru>
2023-08-09 11:47:34 +0500

    решение 1 задачи

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,9 @@ public class Main {
         for (int i =0; i < 10; i ++) {
             System.out.println("Итерация цикла" + i);
         }
+
+        for (int i =1; i <= 10; i ++) {
+            System.out.println("Итерация цикла" + i);
+        }
     }
 }
