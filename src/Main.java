85432e16728611b4b121190a03cd6e260c847adc
Beloyara <beloyara@inbox.ru>
2023-08-09 12:06:46 +0500

    решение 5 задачи

--- src/Main.java
+++ src/Main.java
@@ -21,5 +21,9 @@ public class Main {
         for (int i = 10; i >= -10; i --) {
             System.out.println("Итерация цикла" + i);
         }
+
+        for (int i = 1904; i<= 2096; i= i + 4) {
+            System.out.println(i + "год является високосным");
+        }
     }
 }
